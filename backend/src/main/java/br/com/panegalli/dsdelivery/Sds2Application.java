fd34c59d280a0d51dd30cd6a41808fbd24a8d7b0
Felipe Panegalli <felipe.panegalli@gmail.com>
2021-06-10 21:40:56 -0300

    Started project

---
+++ backend/src/main/java/br/com/panegalli/dsdelivery/Sds2Application.java
@@ -0,0 +1,13 @@
+package br.com.panegalli.dsdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sds2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sds2Application.class, args);
+    }
+
+}
---
+++ backend/src/test/java/br/com/panegalli/dsdelivery/Sds2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.panegalli.dsdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sds2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
